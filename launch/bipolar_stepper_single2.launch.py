cbca0666769f412f0375c0f08e33e7054e68e5c4
robotty1234 <maoroboto@gmail.com>
2022-05-18 10:59:56 +0900

    Add parameter files

---
+++ launch/bipolar_stepper_single2.launch.py
@@ -0,0 +1,14 @@
+from os.path import join
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    pkg_prefix = get_package_share_directory('bipolar_stepper_package')
+    stepper = Node(
+        package='bipolar_stepper_package',
+        executable='bipolar_stepper',
+        name='stepper',
+        parameters=[join(pkg_prefix, 'cfg/parameter2.yaml')]
+    )
+    return LaunchDescription([stepper])
